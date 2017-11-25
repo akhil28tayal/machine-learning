46846e15b1f6b03b6842e40100344f8f50c8b875
Akhil Tayal <akhiltayal2818@gmail.com>
2017-11-26 01:09:23 +0530

    Comments added to Linear Regression

--- Linear-Regression/MultipleLinearRegression.py
+++ Linear-Regression/MultipleLinearRegression.py
@@ -6,6 +6,9 @@ Created on Sat Nov 25 23:23:12 2017
 @author: akhiltayal
 """
 
+# Multiple Linear Regression
+
+# Importing Libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -17,13 +20,16 @@ def importData(filename):
     y = data.iloc[:, -1].values
     return [X, y]
 
+# Feature Scaling for better convergence of the Gradient Descent
+# X = (X - mean)/std
 def featureScaling(X):
     mu = X.mean(0)
     sigma = X.std(0)
     X_norm = (X - mu)/sigma
     return [X_norm, mu, sigma]
 
-
+# Cost Functoin J(theta) = (1/2*m) * sum(h(x) - y)^2
+# h(x) = theta[0] + theta[1] * x (Prediction)
 def computeCost(X, y, theta):
     m = len(X)
     prediction = (theta * X).sum(1)
@@ -31,6 +37,9 @@ def computeCost(X, y, theta):
     J = (sqrError)/(2*m)
     return J
 
+# Gradient Descent theta[j] = theta[j] - derivative(J(theta))
+# theta[j] = theta[j] - (sum(h(theta) - y) * x[j] ) * (alpha/m)
+# x[0] = 1
 def gradientDescent(X, y, theta, alpha, num_iter):
     m = len(X)
     for i in range(0, num_iter):
@@ -42,28 +51,29 @@ def gradientDescent(X, y, theta, alpha, num_iter):
         theta = theta_temp
     return theta
 
+# Graphical Visualisation of the learning algorithm
 def visualisingData(X, y, theta):
     y_pred = (theta * X).sum(1)
     fig = plt.figure()
     axes = fig.add_subplot(111, projection='3d')
-    
+
     axes.scatter(X[:, 1], X[:, 2], y)
     axes.plot(X[:, 1],  X[:, 2], y_pred)
     plt.show()
 
+# Main Function
 if __name__ == "__main__":
     [X, y] = importData('ex1data2.csv')
+
     [X, mu, sigma] = featureScaling(X)
     [y, mu_y, sigma_y] = featureScaling(y)
-    
-    one_array = np.array([1]*len(X))
-    
-    X = np.column_stack((one_array, X))
-    
-    theta = np.array([0] * X.shape[1], dtype = float)
-    
-    theta = gradientDescent(X, y, theta, alpha= 0.01, num_iter = 1500)
-    
-    visualisingData(X, y, theta)
 
-    
+    # Adding vector of 1's for X[0]
+    one_array = np.array([1]*len(X))
+    X = np.column_stack((one_array, X))
+
+    theta = np.array([0] * X.shape[1], dtype = float)
+
+    theta = gradientDescent(X, y, theta, alpha= 0.01, num_iter = 1500)
+
+    visualisingData(X, y, theta)
--- Linear-Regression/SimpleLinearRegression.py
+++ Linear-Regression/SimpleLinearRegression.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 25 23:23:12 2017
+
+@author: akhiltayal
+"""
+
 # Simple Linear Regression
 
 # Importing Libraries
@@ -11,6 +19,8 @@ def importData(filename):
     y = dataset.iloc[:, 1].values
     return [X, y]
 
+# Cost Functoin J(theta) = (1/2*m) * sum(h(x) - y)^2
+# h(x) = theta[0] + theta[1] * x (Prediction)
 def computeCost(X, y, theta):
     m = len(X)
     prediction = theta[0] + (theta[1] * X)
@@ -18,6 +28,9 @@ def computeCost(X, y, theta):
     J = sqrError.sum() / (2*m)
     return J
 
+# Gradient Descent theta[j] = theta[j] - derivative(J(theta))
+# theta[j] = theta[j] - (sum(h(theta) - y) * x[j] ) * (alpha/m)
+# x[0] = 1
 def gradientDescent(X, y, theta, alpha, num_iter):
     m = len(X)
     J = np.array([0] * num_iter, dtype=float)
@@ -29,23 +42,25 @@ def gradientDescent(X, y, theta, alpha, num_iter):
         J[i] = computeCost(X, y, theta)
     return [theta, J]
 
+# Graphical Visualisation of the learning algorithm
 def visualisingData(X, y, theta, J):
     y_pred = theta[0] + theta[1] * X
     plt.scatter(X, y, marker='x', color='red')
     plt.plot(X, y_pred, color='blue')
     plt.show()
-    
+
     plt.plot(J)
     plt.show()
-    
+
+# Main Function
 if __name__ == "__main__":
-    
+
     [X, y] = importData("ex1data1.csv")
-    
+
     theta = np.array([0, 0], dtype=float)
-    
+
     J_zero = computeCost(X, y, theta)
-    
+
     [theta, J] = gradientDescent(X, y, theta, alpha=0.01, num_iter=1500)
-    
+
     visualisingData(X, y, theta, J)
